d7e9df0531b73692d50807e3e89724969ed5c878
yangxiao <921242428@qq.com>
2022-03-11 00:15:15 +0800

    二叉树开始

---
+++ labuladong/二叉树/114. 二叉树展开为链表.py
@@ -0,0 +1,56 @@
+'''
+给你二叉树的根结点 root ，请你将它展开为一个单链表：
+
+展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+展开后的单链表应该与二叉树 先序遍历 顺序相同。
+ 
+
+示例 1：
+
+
+输入：root = [1,2,5,3,4,null,6]
+输出：[1,null,2,null,3,null,4,null,5,null,6]
+示例 2：
+
+输入：root = []
+输出：[]
+示例 3：
+
+输入：root = [0]
+输出：[0]
+ 
+
+提示：
+
+树中结点数在范围 [0, 2000] 内
+-100 <= Node.val <= 100
+'''
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: None Do not return anything, modify root in-place instead.
+        """
+        if root == None:
+            return
+        self.flatten(root.left)
+        self.flatten(root.right)
+        left = root.left
+        right = root.right
+        root.left = None
+        root.right = left
+        ##找到当前右子树的末端
+        p = root
+        while p.right:
+            p = p.right
+        p.right = right
+        return root
